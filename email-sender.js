0a93d83c0a03f06178f6dc56292682a69db861b7
Gladys <gladysalu1@gmail.com>
2024-03-19 11:10:08 +0100

    commit

--- email-sender.js
+++ email-sender.js
@@ -1,6 +1,7 @@
 import nodemailer from 'nodemailer'
 import "dotenv/config"
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
